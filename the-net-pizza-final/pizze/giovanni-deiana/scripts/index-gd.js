e870c07846ebbc2dc5a94cb4c763629e79dccc2b
g1ann0z <giannozcydia@gmail.com>
2023-11-30 00:46:43 +0100

    piatti-generati

--- the-net-pizza-final/pizze/giovanni-deiana/scripts/index-gd.js
+++ the-net-pizza-final/pizze/giovanni-deiana/scripts/index-gd.js
@@ -16,8 +16,44 @@ const buildCard = function(piatto) {
             </div>`;
 }
 
-// <div class="col-12 col-sm-6 col-md-4 py-2">
-  const buildSection = function(menu, category) {
+const generazioneCasuale = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const min = 1;
+const max = 14;
+const numPiatti = 3;
+
+const generazionePiatti = (min, max, n) => {
+  const idCasuali = [];
+  while (idCasuali.length < n) {
+    const newId = generazioneCasuale(min, max);
+    if (!idCasuali.includes(newId)) {
+      idCasuali.push(newId);
+    }
+  }
+  return idCasuali;
+};
+
+const piatti = generazionePiatti(min, max, numPiatti);
+
+const buildSection = function(menu, piatti) {
+  const divParentElement = document.getElementById('piatti');
+
+  for(let id of piatti){
+    const piattoScelto = menu.find(item => item.id === id);
+    if(piattoScelto) {
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-4');
+      cardElement.innerHTML = buildCard(piattoScelto);
+      divParentElement.appendChild(cardElement);
+    }
+  }
+}
+
+const buildMenu = function(menu) {
+  buildSection(menu, piatti);
+}
+
+/*const buildSection = function(menu, category) {
     const categoryItems = menu.filter(x => x.category === category);
     const divParentElement = document.getElementById(category);
 
@@ -31,9 +67,7 @@ const buildCard = function(piatto) {
 
 const buildMenu = function(menu) {
     buildSection(menu, 'antipasti');
-    buildSection(menu, 'primi');
-    buildSection(menu, 'dolci'); 
-}
+}*/
 
 fetch('http://my-json-server.typicode.com/michelefenu/tnv-academy-XI/piatti/')
     .then(res => res.json())
