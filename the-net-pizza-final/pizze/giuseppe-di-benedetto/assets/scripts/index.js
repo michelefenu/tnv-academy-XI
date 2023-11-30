174da9d48aa9a85608acd685642f63e80ad2a63c
Giuseppe Di Benedetto <giuseppedibenedetto654@gmail.com>
2023-11-30 13:16:12 +0100

    codice-formattato

--- the-net-pizza-final/pizze/giuseppe-di-benedetto/assets/scripts/index.js
+++ the-net-pizza-final/pizze/giuseppe-di-benedetto/assets/scripts/index.js
@@ -1,32 +1,31 @@
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) ) + min;
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-const buildCard = function(piatto){
+const buildCard = function (piatto) {
   return `<div class="wiki" >
-             <a class="link" href="../../../../index.html">
-             <div class="info-card">
-               <img src="${piatto.imageUrl}" class="img-fluid" style = "border-radius: 8px;"
+            <a class="link" href="../../../../index.html">
+              <div class="info-card">
+                <img src="${piatto.imageUrl}" class="img-fluid" style = "border-radius: 8px;"
                   alt="${piatto.title}">
-              <div class="card-body">
-                  <h5 class="card-title">${piatto.title} - ${piatto.price}€</h5>
-                  <p class="card-text">${piatto.description}</p>
+                    <div class="card-body">
+                      <h5 class="card-title">${piatto.title} - ${piatto.price}€</h5>
+                        <p class="card-text">${piatto.description}</p>
+                    </div>
               </div>
-          </div>
-      </div>
-     </a>
-  </div>`;
+            </a>
+          </div>`;
 }
 
-const buildSection = function(menu, numbersOfcards){
-  
+const buildSection = function (menu, numbersOfcards) {
+
   const divParentElement = document.getElementById('dishes');
   const selectedIds = [];
 
   //<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5" role="listitem">
-  for(let i = 0; i<numbersOfcards; i++){
+  for (let i = 0; i < numbersOfcards; i++) {
     let randomIndex;
-    do{
+    do {
       randomIndex = randomNumber(1, menu.length);
-    }while(selectedIds.includes(menu[randomIndex].id));
+    } while (selectedIds.includes(menu[randomIndex].id));
 
     selectedIds.push(menu[randomIndex].id)
 
@@ -34,7 +33,7 @@ const buildSection = function(menu, numbersOfcards){
 
     const cardElement = document.createElement('div');
     cardElement.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'mb-5');
-    cardElement.innerHTML = buildCard(randomDishes); 
+    cardElement.innerHTML = buildCard(randomDishes);
     divParentElement.appendChild(cardElement);
   }
 }
@@ -42,6 +41,6 @@ const buildSection = function(menu, numbersOfcards){
 
 
 fetch('http://my-json-server.typicode.com/michelefenu/tnv-academy-XI/piatti')
-.then(res => res.json())
-.then(res => buildSection(res, 3))
-.catch(err => console.error('Errore',err))
+  .then(res => res.json())
+  .then(res => buildSection(res, 3))
+  .catch(err => console.error('Errore', err))
